21431dccf7e57eb277b5ac5ab384c28841448eed
maleks1983 <maleks1983@gmail.com>
2021-05-25 23:19:50 +0300

    HW4
    Signed-off-by: maleks1983 <maleks1983@gmail.com>

---
+++ Lesson 7/index.js
@@ -0,0 +1,14 @@
+function getNumbers(from, to) {
+
+    for (let i = from; i <= to; i++) {
+        console.log(result);
+    }
+
+}
+
+//HW1.4
+function inputNumber() {
+    log from = prompt('Get first number');
+    log to = prompt('Get second number');
+
+}
